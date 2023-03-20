e06cfd098982d835dfabdff4fb34760972bb7dbc
henrique <henrique-lourenco@hotmail.com>
2023-03-20 14:56:06 -0300

    teste de commit direto da ide Eclipes

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		
+		/*// TODO Auto-generated method stub
+		System.out.println("Hello Word!");*/
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Familia
+ *
+ */
+module primeiroprogramajava {
+}
