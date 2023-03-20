b47f5a998ed1277fb50cfe70b531bcbb186d785f
henrique <henrique-lourenco@hotmail.com>
2023-03-20 16:25:08 -0300

    criação da calculadora.

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -1,12 +1,12 @@
 package br.com.dio;
 
-import br.com.dio.model.Gato;
+import br.com.dio.model.Gatos;
 
 public class primeiroprograma {
 
 	public static void main(String[] args) {
 
-		Gato gato = new Gato();
+		Gatos gato = new Gatos();
 		
 		/*// TODO Auto-generated method stub
 		System.out.println("Hello Word!");*/
